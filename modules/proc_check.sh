4cfae6d5bd1d2e0abb9d1fa6322cc307024a32c0
whilte1ight <qortjdghks7@gmail.com>
2025-06-12 18:34:40 +0900

    Add files via upload
    초안 commit

---
+++ modules/proc_check.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 의심 프로세스 및 파일, 경로
+SUSPICIOUS_NAMES_PATHS=(
+    "hpasmmld"
+    "smartadm"
+    "hald-addon-volume"
+    "dbus-srv"
+    "gm"
+    "rad$"
+    "/dev/shm/."
+    "/tmp/."
+)
+
+check_suspicious_processes_and_files() {
+    gen_log "INFO: 의심스러운 프로세스 및 파일,디렉토리 검사 시작"
+    local found_suspicious_item=false
+    local LIMITED_SEARCH_PATHS=("/tmp" "/var/tmp" "/dev/shm" "/etc" "/run" "/usr/local/bin" "/usr/local/sbin")
+
+    for pattern in "${SUSPICIOUS_NAMES_PATHS[@]}"; do
+        # 프로세스 검사
+        if pgrep -fli "$pattern" &>/dev/null; then
+            gen_log "WARN: 의심 프로세스 발견 - 패턴: $pattern"
+            pgrep -fli "$pattern" | while read -r line; do gen_log "  프로세스: $line"; done
+            found_suspicious_item=true
+        fi
+
+        # 파일 검사
+        for s_path in "${LIMITED_SEARCH_PATHS[@]}"; do
+            [ ! -d "$s_path" ] && continue
+            find "$s_path" -name "$pattern" -print0 2>/dev/null | while IFS= read -r -d $'\0' found_file; do
+                gen_log "WARN: 의심 파일 발견 - $found_file"
+                found_suspicious_item=true
+            done
+        done
+    done
+
+    if [ "$found_suspicious_item" = false ]; then
+        gen_log "INFO: 의심 프로세스 및 파일 없음."
+    fi
+}
