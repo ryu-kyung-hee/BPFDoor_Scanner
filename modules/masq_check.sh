094222900c3a1e58a040dea419ec92807677b933
SJH0401 <deamondsjh@gmail.com>
2025-06-14 19:25:54 +0900

    Update masq_check.sh

--- modules/masq_check.sh
+++ modules/masq_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 check_process_masquerading() {
-    gen_log "INFO: 위장 프로세스 확인 중"
+    echo "INFO: 위장 프로세스 확인 중"
     local found_masquerade=false
     local comm_name exe_name
 
@@ -17,14 +17,15 @@ check_process_masquerading() {
         gen_log "WARN: 프로세스명 불일치 탐지"
         gen_log " -> In-Memory Name : $comm_name"
         gen_log " -> Executable File : $exe_name"
-        gen_log " -> Full Command: $(cat /proc/$pid/cmdline | tr -d '\0')"
+        gen_log " -> Full Command: $(cat /proc/$pid/cmdline 2>/dev/null | tr -d '\0')"
         found_masquerade=true
     fi
   done
 
 if [ "$found_masquerade" = false ]; then
-    gen_log "INFO: 위장 프로세스가 발견되지 않음."
+    echo "INFO: 위장 프로세스가 발견되지 않음."
 fi
 }
 
+check_process_masquerading
 echo "위장 프로세스 검사가 종료됨."
