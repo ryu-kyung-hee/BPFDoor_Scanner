21f45b15b86349316a8b8284dd18bc07fd966465
seo0e <yoonsy1025@naver.com>
2025-06-18 22:09:38 +0900

    250618 revise

--- modules/c2_check.sh
+++ modules/c2_check.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-#KISA에서 발표된 공격자  IP IOC(현재는 유일한 IP IOC다.)
-C2_IP="165.232.174.130"
+#필요시 변경. 현재 가상환경 공격자 IP로 설정
+C2_IP="192.168.100.3"
 
 check_c2_ip_connection() {
-	gen_log "INFO: 연결된 C2 IP 주소 검사 시작 (C2 IP: $C2_IP)..."
-	local found_c2_connection=false
-	if command -v ss &>/dev/null; then
-		if ss -ntp | grep -q "$C2_IP"; then
-			gen_log "WARN: C2 IP ($C2_IP) 주소에 대한 네트워크 연결 발견"
-			ss -ntp | grep "$C2_IP" | while read -r line; do gen_log " 	Connection: $line"; done
-			found_c2_connection=true
-		fi
-	elif command -v netstat &>/dev/null; then
-		if netstat -ntp | grep -q "$C2_IP"; then
-			gen_log "WARN: C2 IP ($C2_IP) 주소에 대한 네트워크 연결 발견"
-			netstat -ntp | grep "$C2_IP" | while read -r line; do gen_log "		Connection: $line"; done
-		fi
-	else
-		gen_log "WARN: 'ss' 또는 'netstat' 명령을 찾을 수 없으므로 네트워크 연결 검사를 건너뛸 수 있음."
+    gen_log "INFO: C2 IP 연결 점검 시작"
+    local found_c2_connection=false
 
+
+    #ss 명령어 검사
+    if command -v ss &>/dev/null; then
+	if ss -ntup | grep -q "$C2_IP"; then
+		found_c2_connection=true
+	        ss -ntup | grep "$C2_IP" | while read -r line; do
+               		local proto=$(echo "$line" | awk '{print $1}')
+                	local local_addr=$(echo "$line" | awk '{print $5}')
+                	local remote_addr=$(echo "$line" | awk '{print $6}')
+                	local pid=$(echo "$line" | grep -oP 'pid=\K[0-9]+' | sort -u)
+                	local cmd=$(echo "$line" | grep -oP 'users:\(\("\K[^"]+' | sort -u)
+
+                	gen_log "WARN: C2 IP ($C2_IP) 주소에 대한 네트워크 연결 발견" 
+                	gen_log "$proto: $local_addr → $remote_addr (PID: ${pid:-N/A}, CMD: ${cmd:-N/A})"
+               done
+    	fi
+
+    #ss 명령어가 존재하지 않을 시 netstat 명령어 검사
+    elif command -v netstat &>/dev/null; then
+	if netstat -ntup 2>/dev/null | grep "$C2_IP"; then
+		found_c2_connection=true
+		netstat -ntup 2>/dev/null | grep "$C2_IP" | while -r line; do
+            		gen_log " netstat: $line"
+                done
 	fi
-    	[ "$found_c2_connection" = false ] && gen_log "INFO: C2 IP ($C2_IP) 에 대해 활성화된 연결 없음."
+    else
+        gen_log "WARN: 'ss'와 'netstat' 명령어가 존재하지 않음"
+    fi
+
+
+    if [ "$found_c2_connection" = false ]; then
+        gen_log "INFO: 연결된 C2 IP 없음"
+    fi
 }
-
-
